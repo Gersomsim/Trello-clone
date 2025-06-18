2e41ddef55817f5ff4b9f11209577da5c634af15
Gersomsim <gersomhernandez@me.com>
2025-06-18 01:29:19 -0600

    Agregar nuevos tipos de variantes al componente UiButton

--- src/app/ui/components/atoms/ui-button/ui-button.component.ts
+++ src/app/ui/components/atoms/ui-button/ui-button.component.ts
@@ -29,13 +29,20 @@ export class UiButtonComponent {
 
 	getButtonClasses() {
 		const baseClasses =
-			'inline-flex items-center justify-center rounded-md text-sm font-medium focus:outline-none'
+			'inline-flex items-center justify-center text-sm font-medium focus:outline-none'
 		const variantClasses = {
 			primary:
 				'bg-blue-500 text-white hover:bg-blue-600 dark:bg-blue-600 dark:hover:bg-blue-700',
 			secondary: 'bg-gray-500 text-white hover:bg-gray-600',
 			outline: 'border border-gray-300 text-gray-700 hover:bg-gray-50',
 			ghost: 'text-gray-700 bg-gray-50  dark:text-gray-400 dark:bg-gray-700',
+			danger: 'bg-red-500 text-white hover:bg-red-600',
+			success: 'bg-green-500 text-white hover:bg-green-600',
+			warning: 'bg-yellow-500 text-white hover:bg-yellow-600',
+			info: 'bg-blue-500 text-white hover:bg-blue-600',
+			light: 'bg-gray-500 text-white hover:bg-gray-600',
+			indigo: 'bg-indigo-500 text-white hover:bg-indigo-600',
+			'outline-indigo': 'border border-indigo-500 text-indigo-500 hover:bg-indigo-50',
 		}
 
 		const sizeClasses = {
--- src/app/ui/components/types/button.type.ts
+++ src/app/ui/components/types/button.type.ts
@@ -1,6 +1,17 @@
 import { SizeType } from './size.type'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'outline' | 'ghost'
+export type ButtonVariant =
+	| 'primary'
+	| 'secondary'
+	| 'outline'
+	| 'ghost'
+	| 'danger'
+	| 'success'
+	| 'warning'
+	| 'info'
+	| 'light'
+	| 'indigo'
+	| 'outline-indigo'
 export type ButtonSize = SizeType
 export type ButtonType = 'button' | 'submit' | 'reset'
 export type ButtonRounded = 'sm' | 'md' | 'lg' | 'full' | 'none'
